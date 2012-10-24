0badce519b1746497523590561c5666bf64ab662
thex1le <thex1le@gmail.com>
2012-10-24 15:30:52 -0400

    initial commit of source code for py80211

---
+++ Parse80211.py
@@ -0,0 +1,304 @@
+import pcap
+import sys
+import struct
+import pdb
+
+
+class IeTag80211:
+    """
+    Parsing 802.11 frame information elements
+    """
+    def __init__(self):
+        """
+        build parser for IE tags
+        """
+        self.tagdata = {"unparsed":[]}  # dict to return parsed tags
+        self.parser = {
+            "\x00": self.ssid,  # ssid IE tag parser
+            "\x01": self.rates,  # data rates tag parser
+            "\x03": self.channel,  # channel tag parser
+            "\x30": self.rsn,  # rsn tag parser
+            "\x32": self.exrates  # extended rates tag parser
+                      }
+
+    def parseIE(self, rbytes):
+        """
+        takes string of raw bytes splits them into tags
+        passes those tags to the correct parser
+        retruns parsed tags as a dict, key is tag number
+        rbytes = string of bytes to parse
+        """
+        self.tagdata = {"unparsed":[]}  # dict to return parsed tags
+        offsets = {}
+        while len(rbytes) > 0:
+            fbyte = rbytes[0]
+            # add two to account for size byte and tag num byte
+            blen = ord(rbytes[1]) + 2  # byte len of ie tag
+            if fbyte in self.parser.keys():
+                self.parser[fbyte](rbytes[0:blen])
+            else:
+                # we have no parser for the ie tag
+                self.tagdata["unparsed"].append(rbytes[0:blen])
+            rbytes = rbytes[blen:]  # add two to offset tag number and get to new one 
+       
+    def exrates(self, rbytes):
+        """
+        parses extended supported rates
+        exrates IE tag number is 0x32
+        retruns exrates in a list
+        """
+        exrates = []
+        for exrate in tuple(rbytes[2:]):
+            exrates.append(ord(exrate))
+        self.tagdata["exrates"] = exrates
+
+    def channel(self, rbytes):
+        """
+        parses channel
+        channel IE tag number is 0x03
+        returns channel as int
+        last byte is channel
+        """
+        self.tagdata["channel"] = ord(rbytes[2])
+
+    def ssid(self, rbytes):
+        """
+        parses ssid IE tag
+        ssid IE tag number is 0x00
+        returns the ssid as a string
+        """
+        # how do we handle hidden ssids?
+        self.tagdata["ssid"] = unicode(rbytes[2:], errors='replace')
+
+    def rates(self, rbytes):
+        """
+        parses rates from ie tag
+        rates IE tag number is 0x01
+        returns rates as in a list
+        """
+        rates = []
+        for rate in tuple(rbytes[2:]):
+            rates.append(ord(rate))
+        self.tagdata["rates"] = rates
+
+    def rsn(self, rbytes):
+        """
+        parses robust security network ie tag
+        rsn ie tag number is 0x30
+        returns rsn info in nested dict
+        gtkcs is group temportal cipher suite
+        akm is auth key managment, ie either wpa, psk ....
+        ptkcs is pairwise temportal cipher suite
+        """
+        rsn = {}
+        ptkcs = []
+        akm = []
+        # need to extend this
+        cipherS = {
+            1 : "WEP-40/64",
+            2 : "TKIP",
+            3 : "RESERVED",
+            4 : "CCMP",
+            5 : "WEP-104/128"
+            }
+        authKey = {
+            1 : "802.1x or PMK",
+            2 : "PSK",
+            }
+        version = struct.unpack('h', rbytes[2:4])[0]
+        rsn["gtkcsOUI"] = rbytes[4:7]
+        # GTK Bytes Parsing
+        gtkcsTypeI = ord(rbytes[7])
+        if gtkcsTypeI in cipherS.keys():
+            gtkcsType = cipherS[gtkcsTypeI]
+        else:
+            gtkcsType = gtkcsTypeI
+        rsn["gtkcsType"] = gtkcsType
+        # PTK Bytes Parsing
+        # len of ptk types supported
+        ptkcsTypeL = struct.unpack('h', rbytes[8:10])[0]
+        counter = ptkcsTypeL
+        cbyte = 10 #current byte
+        while counter >= ptkcsTypeL:
+            ptkcsTypeOUI = rbytes[cbyte:cbyte+3]
+            ptkcsTypeI = ord(rbytes[cbyte+3])
+            if ptkcsTypeI in cipherS.keys():
+                ptkcsType = cipherS[ptkcsTypeI]
+            else:
+                ptkcsType = ptkcsTypeI
+            cbyte += 4 # end up on next byte to parse
+            ptkcs.append({"ptkcsOUI":ptkcsTypeOUI,
+                          "ptkcsType":ptkcsType})
+            counter -= 1
+
+        akmTypeL = struct.unpack('h', rbytes[cbyte:cbyte+2])[0]
+        cbyte += 2
+        counter = akmTypeL
+        #this might break need testing
+        while counter >= akmTypeL:
+            akmTypeOUI = rbytes[cbyte:cbyte+3]
+            akmTypeI = ord(rbytes[cbyte+3])
+            if akmTypeI in authKey.keys():
+                akmType = authKey[akmTypeI]
+            else:
+                akmType = akmTypeI
+            cbyte += 4 # end up on next byte to parse
+            akm.append({"akmOUI":akmTypeOUI,
+                          "akmType":akmType})
+            counter -= 1
+        # 8 bits are switches for various features
+        capabil = rbytes[cbyte:cbyte+2]
+        cbyte += 3 # end up on PMKID list
+        rsn["pmkidcount"] = rbytes[cbyte:cbyte +2]
+        rsn["pmkidlist"] = rbytes[cbyte+3:]
+        rsn["ptkcs"] = ptkcs
+        rsn["akm"] = akm
+        rsn["capabil"] = capabil
+        self.tagdata["rsn"] = rsn
+
+class Parse80211:
+    """
+    Class file for parsing
+    several common 802.11 frames
+    """
+    def __init__(self, dev):
+        """
+        open up the libpcap interface
+        open up the device to sniff from
+        dev = device name as a string
+        """
+        self.IE = IeTag80211()
+        self.rt = 18  # radio tap header offset 18 bytes in
+        # bytes are in little endian hex
+        # doing this based off bytes is wrong... it works mostly however subtype is in bits not bytes and this may break in the future, should run bitwise on the byte
+        self.parser = {
+            "\x00": self.placedef,  # assoication request 
+            "\x10": self.placedef,  # assoication responce
+            "\x20": self.placedef,  # reassoication request
+            "\x30": self.placedef,  # reassoication response
+            "\x40": self.probeReq,  # probe request
+            "\x50": self.placedef,  # probe response
+            "\x80": self.beacon,    # Beacon
+            "\x90": self.placedef,  # ATIM
+            "\xA0": self.placedef,  # disassoication
+            "\xB0": self.placedef,  # Authenticaiton
+            "\xC0": self.placedef,  # Deauthentication
+            "\xC8": self.qos,       # qos
+            "\x08": self.data,      # data
+        }
+        self.lp = pcap.pcapObject()
+        # check what these numbers mean
+        self.lp.open_live(dev, 1600, 0 ,100)
+    
+    def placedef(self, data):
+        print "No parser for subtype"
+
+    def getFrame(self):
+        """
+        return a frame from libpcap
+        """
+        return self.lp.next()
+
+    def parseFrame(self, frame):
+        """
+        Determine the type of frame and
+        choose the right parser
+        """
+        if frame != None:
+            psize = frame[0]
+            data = frame[1]
+            tstamp = frame[2]
+        else:
+            return None
+        # determine frame subtype
+        # subtype should be one off radio tap headers
+        subtype = data[self.rt: self.rt +1]
+        if subtype in self.parser.keys():
+            return self.parser[subtype](data)
+        else:
+            # we dont have a parser for the packet
+            return None
+    
+    def data(self, data):
+        """
+        parse the src,dst,bssid from a data frame
+        subtype = \x08 data hex byte
+        wireshark shows subtype as \x20
+        """
+        # fix bug in case we dont get radio tap headers
+        # need to parse ds flags
+        # do a bit bitwise & to check which of the last 2 bits are set
+        dsbits = ord(data[20]) & 3
+        dst = data[22:22 + 6]  # destination addr 6 bytes
+        src = data[34:34 + 6]  # source addr 6 bytes
+        bssid = data[28:28 + 6]  # bssid addr 6 bytes
+        return {"key":"\x08", "src":src, "dst":dst, "bssid":bssid, "ds":dsbits}
+
+    def qos(self, data):
+        """
+        parse the src,dst,bssid from a qos frame
+        subtype = \xC8
+        """
+        # fix bug in case we dont get radio tap headers
+        dsbits = ord(data[20]) & 3
+        dst = data[22:22 + 6]  # destination addr 6 bytes
+        src = data[34:34 + 6]  # source addr 6 bytes
+        bssid = data[28:28 + 6]  # bssid addr 6 bytes
+        return {"key":"\xC8", "src":src, "dst":dst, "bssid":bssid, "ds":dsbits}
+
+    def probeReq(self, data):
+        """
+        Parse out probe requests
+        return a dict of with keys of
+        src, dst, bssid, probe request
+        """
+        # fix bug in case we dont get radio tap headers
+        dsbits = ord(data[20]) & 3
+        dst = data[22:22 + 6]  # destination addr 6 bytes
+        src = data[28:28 + 6]  # source addr 6 bytes
+        bssid = data[34:34 + 6]  # bssid addr 6 bytes
+        # parse the IE tags
+        # possible bug, no fixed 12 byte paramaters before ie tags?
+        self.IE.parseIE(data[42:])
+        if "key" not in self.IE.tagdata.keys():
+            essid = ""
+        else:
+            essid = self.IE.tagdata["ssid"]
+        if "channel" not in self.IE.tagdata.keys():
+            channel = ""
+        else:
+            channel = self.IE.tagdata["channel"]
+        return {"key":"\x50", "bssid":bssid, "essid":essid, "src":src, 
+            "dst":dst, "channel":channel, "extended":self.IE.tagdata, "ds":dsbits}
+    
+    def beacon(self, data):
+        """
+        Parse out beacon packets
+        return a dict with the keys of
+        src, dst, bssid, essid, channel ....
+        going to need to add more
+        """
+        # fix bug in case we dont get radio tap headers
+        dst = data[22:22 + 6]  # destination addr 6 bytes
+        src = data[28:28 + 6]  # source addr 6 bytes
+        bssid = data[34:34 + 6]  # bssid addr 6 bytes
+        # parse the IE tags
+        self.IE.parseIE(data[54:])
+        if "ssid" not in self.IE.tagdata.keys():
+            essid = ""
+        else:
+            essid = self.IE.tagdata["ssid"]
+        if "channel" not in self.IE.tagdata.keys():
+            channel = ""
+        else:
+            channel = self.IE.tagdata["channel"]
+        return {"key":"\x80", "bssid":bssid, "essid":essid, "src":src, "dst":dst, "channel":channel, "extended":self.IE.tagdata}
+
+if __name__ == "__main__":
+    x = Parse80211(sys.argv[1])
+    while True:
+        frame = x.parseFrame(x.getFrame())
+        if frame != None:
+            if frame["key"] == "\x50":
+                print frame
+        #print x.parseFrame(x.getFrame())
---
+++ Tool80211.py
@@ -0,0 +1,207 @@
+import threading
+import time
+# custom imports
+import Parse80211
+import PyLorcon2
+
+#debug imports
+import pdb
+import sys
+import os
+
+class Toolkit80211:
+    """
+    Group of class's for working with 80211
+    """
+    def __init__(self,interface):
+        """
+        interface = string 
+        currently assumes all cards are to be opened in monitor mode
+        """
+        # open the card up and gain a a context to them
+        # create a dict with interface name and context
+        self.moniface = {"ctx":PyLorcon2.Context(interface)}
+        # place cards in injection/monitor mode
+        self.moniface["ctx"].open_injmon()
+        self.moniface["name"] = self.moniface["ctx"].get_vap()
+
+    class ChannelHop(threading.Thread):
+        """
+        Control a card and cause it to hop channels
+        Only one card per instance
+        """
+        def __init__(self,interface):
+            """
+            set the channel hopping sequence
+            expects lorcon injmon() context
+            """
+            threading.Thread.__init__(self)
+            threading.Thread.daemon = True
+            self.iface = interface
+            # dwell for 3 time slices on 1 6 11
+            # default is 3/10 of a second
+            # got the lists from kismet config file
+            # thanks dragorn!
+            self.channellist = [1,6,11,14,2,7,3,8,4,9,5,10,36,40,44,48,52,56,60,64,149,153,157,161,165]
+            self.hopList = []
+            self.current = 0
+            self.checkChannels()
+
+        def checkChannels(self):
+            """
+            card drivesr suck, determine what channels 
+            a card supports before we start hopping
+            """
+            # try setting 802.11ab channels first
+            # this may not work depending on 5ghz dfs
+            # reverse so we start with 5ghz channels first
+            for ch in self.channellist:
+                try:
+                    self.iface.set_channel(ch)
+                except PyLorcon2.Lorcon2Exception:
+                    continue
+                self.hopList.append(ch)
+
+        def hop(self, dwell=.4):
+            """
+            Hop channels
+            """
+            while True:
+                for ch in self.hopList:
+                    try:
+                        self.iface.set_channel(ch)
+                    except PyLorcon2.Lorcon2Exception:
+                        continue
+                    self.current = ch
+                    if ch in [1,6,11]:
+                        # dwell for 4/10 of a second
+                        # we want to sit on 1 6 and 11 a bit longer
+                        time.sleep(dwell)
+                    else:
+                        time.sleep(.2)
+        
+        def run(self):
+            self.hop()
+
+    class Airview(threading.Thread):
+        """
+        Grab a snapshot of the air
+        whos connected to who
+        whats looking for what
+        # note right now expecting to deal with only one card
+        # will need to refactor code to deal with more then one in the future
+        # dong this for time right now
+        """
+        def __init__(self, interface):
+            """
+            Open up a packet parser for a given interface
+            Thread the instance
+            """
+            threading.Thread.__init__(self)
+            threading.Thread.daemon = True
+            # get interface name for use with pylibpcap
+            self.iface = interface["name"]
+            # get context for dealing with channel hopper
+            self.ctx = interface["ctx"]
+            # open up a parser
+            self.rd = Parse80211.Parse80211(self.iface)
+            # key = bssid, value = essid
+            self.bss = {}
+            # load up latest beacon packet for an AP
+            self.apData = {}
+            # note fake AP's can end up in here.. need to solve this
+            # key = essid, value [bssid, bssid]
+            self.ess = {}
+            # this may not work for WDS, though ignoring wds for now
+            # key = mac, value=assoication
+            self.clients = {}
+            # data about a given client
+            self.clientData = {}
+        
+        def updateClient(self, frame):
+            """
+            Update self.clients var based on ds bits
+            """
+            bssid = frame["bssid"]
+            src = frame["src"]
+            dst = frame["dst"]
+            ds = frame["ds"]
+            if ds == 0:
+                # broadcast/adhoc
+                self.clients[src] = "Not Assoicated"
+            elif ds == 1:
+                # station to ap
+                self.clients[src] = dst
+                return
+            elif ds == 2:
+                # ap to station
+                self.clients[dst] = src
+                return
+            elif ds == 3:
+                # wds, were ignoring this for now
+                return
+            else:
+                return
+
+        def parse(self):
+            """
+            Grab a packet, call the parser then update
+            The airview state vars
+            """
+            while True:
+                self.channel = self.hopper.current
+                frame = self.rd.parseFrame(
+                            self.rd.getFrame())
+                # beacon frames
+                if frame == None:
+                    continue
+                if frame["key"] == "\x80":
+                    bssid = frame["bssid"]
+                    essid = frame["essid"]
+                    # update bss list, we dont check for keys
+                    # as it allows the essid for a given bssid to be updated
+                    self.bss[bssid] = essid
+                    # update ess list
+                    if essid in self.ess.keys():
+                        self.ess[essid].append(bssid)
+                    else:
+                        self.ess[essid]=[bssid]
+                    #update all info about ap
+                    self.apData[bssid] = frame
+                    continue
+                # data frames
+                elif frame["key"] in ["\x08", "\xC8","\x50"]:
+                    self.updateClient(frame)
+                elif frame["key"] in ["\x50"]:
+                    self.clientData['src'] = frame
+
+        def run(self):
+            """
+            start the parser
+            """
+            # need to start channel hopping here
+            self.hopper = Toolkit80211.ChannelHop(self.ctx)
+            self.hopper.start()
+            self.parse()
+
+if __name__ == "__main__":
+    try:
+        x = Toolkit80211(sys.argv[1])
+        #will only work with one
+        y = x.Airview(x.moniface)
+        y.start()
+        while True:
+            time.sleep(2)
+            os.system("clear")
+            lbss = y.bss
+            print "Channel %i" %(y.channel)
+            print "Access point"
+            for bssid in lbss.keys():
+                print "%s %s" %(bssid.encode('hex'), lbss[bssid])
+            print "\nClients"
+            lclient = y.clients
+            for client in lclient.keys():
+                print "%s %s" %(client.encode('hex'), lclient[client]) 
+    except KeyboardInterrupt:
+        print "\nbye"
+        sys.exit(0)
