af9b1313532ca6733107786d1dd81a0b2d005ef4
David Francos (XayOn) <opensource@davidfrancos.net>
2012-12-06 12:42:11 +0100

    Modified doc file structure

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 
 from distutils.core import setup
 
-setup(name='py80211',
+setup(
+    name='py80211',
     version='1.0',
     description='Lib suite for reading/writing 80211 packets',
     author='Textile & Crypt0s',
     license='GPL2',
     classifiers=[ 'Development Status :: 4 - Beta'],
-    py_modules = ['Parse80211', 'Tool80211', 'Gen80211'],
-    )
+    py_modules = ['py80211']
+)
