f5964b2f48082f2a72ba1b00cb2e16be83c67b2f
thex1le <thex1le@gmail.com>
2012-12-06 12:00:36 -0500

    fixed packet building

--- Gen80211.py
+++ Gen80211.py
@@ -114,7 +114,7 @@ class packetGenerator:
         """
         # packetParts positions are as follows 
         # 0:bptype 1:destination_addr 2:source_addr 3:bss_id_addr 4:reason
-        packet = self.genPtype(bptype) # packet subtype & flags
+        packet = [self.genPtype(bptype)] # packet subtype & flags
         packet.append('\x00\x00')        # duration
         packet.append(dstAddr)       # destain_addr
         packet.append(srcAddr)       # source_addr
@@ -218,7 +218,7 @@ class packetGenerator:
         """
         # packetParts positions are as follows 
         # 0:type 1:destination_addr 2:source_addr 3:bss_id_addr 4:reason
-        packet = self.genPtype(btype)  # subtype & flags
+        packet = [self.genPtype(btype)]  # subtype & flags
         packet.append('\x00\x00')        # duration
         packet.append(dstAddr)       # destain_addr
         packet.append(srcAddr)       # source_addr
@@ -233,7 +233,7 @@ class packetGenerator:
         """
         # packetParts positions are as follows 
         # 0:type 1:destination_addr 2:source_addr 3:bss_id_addr 4:reason
-        packet = self.genPtype(btype) # subtype & flags
+        packet = [self.genPtype(btype)] # subtype & flags
         packet.append('\x00\x00')    # duration
         packet.append(dstAddr)       # destain_addr
         packet.append(srcAddr)       # source_addr
